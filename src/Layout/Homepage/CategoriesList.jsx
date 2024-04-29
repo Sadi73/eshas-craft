0e4de9599bcdcdcf0d40c079ca355d8203c18001
Sadi Muhammad Tanzim <saditanzim@gmail.com>
2024-04-30 00:22:24 +0600

    swiper slider added

--- src/Layout/Homepage/CategoriesList.jsx
+++ src/Layout/Homepage/CategoriesList.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 import { TbSquareAsterisk } from 'react-icons/tb';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+
+import { EffectCoverflow, Pagination } from 'swiper/modules';
 
 const categories = ['Wooden Furniture & Sculptures', 'Wooden Home Decor', 'Wooden Utensils and Kitchenware', 'Jute Home Decor', 'Jute Kitchenware & utensils', 'Jute and wooden jewellery']
 
@@ -9,35 +17,33 @@ const CategoriesList = () => {
             <h1 className='text-[#b18b5e] trirong-font text-5xl text-center'>Categories</h1>
             <p className='text-center'>------ You can find these kind of crafts ------</p>
 
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 max-w-[90%] mx-auto'>
-                {categories.map(caregory =>
-                    <div key={caregory?._id} data-aos="fade-up">
-                        <div className="card card-compact  bg-base-100 shadow-xl h-[500px]">
-                            <figure><img src={caregory?.image} className='w-full h-64' alt="caregory-image" /></figure>
-                            <p className='absolute bg-green-500 text-white py-2 px-4 font-bold rounded-lg top-2 right-2'>{caregory?.status}</p>
-                            <div className="card-body" >
-                                <p>{caregory?.price}</p>
-                                <div className='flex items-center justify-between'>
-                                    <h2 className="card-title">{caregory?.estate_title}</h2>
-                                    <p className='grow-0 text-green-500'>{caregory?.segment_name}</p>
-                                </div>
-                                <div className='flex items-center gap-2'>
-                                    <TbSquareAsterisk />
-                                    <p>{caregory?.area}</p>
-                                </div>
-                                <p>{caregory?.description}</p>
-                                <div className="">
-                                    <button className="btn bg-green-500 text-white hover:bg-green-600 w-full"
-                                        // onClick={() => navigate(`details/${caregory?.id}`)}
-                                    >View Property</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                )
-                }
+            <Swiper
+                effect={'coverflow'}
+                grabCursor={true}
+                centeredSlides={true}
+                slidesPerView={'auto'}
+                coverflowEffect={{
+                    rotate: 50,
+                    stretch: 0,
+                    depth: 100,
+                    modifier: 1,
+                    slideShadows: true,
+                }}
+                pagination={true}
+                modules={[EffectCoverflow, Pagination]}
+                className="mySwiper"
+            >
+                {/* <SwiperSlide>
+                    <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+                </SwiperSlide> */}
+                {categories.map((category, index) =>
+                    <SwiperSlide key={index} >
+                        <span className={`category-${index} bg-red-300 w-[500px] h-[300px] flex justify-center items-center text-xl`}>
+                            <h1>{category}</h1>
+                        </span>
+                    </SwiperSlide>)}
 
-            </div>
+            </Swiper>
         </div>
     );
 };
